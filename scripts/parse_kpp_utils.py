cbbde76fc91abb6ab7b3613a106a837eb95a0e50
David Fillmore <fillmore@ucar.edu>
2023-12-05 09:40:41 -0700

    Added doc comments to parse_kpp_utils module.

--- scripts/parse_kpp_utils.py
+++ scripts/parse_kpp_utils.py
@@ -11,13 +11,32 @@ import logging
 
 
 def is_float(string):
+    """
+    Test if a string can be converted to a float
+
+    Parameters
+        (str) string: test string
+
+    Returns
+        (bool): True if convertible float, False otherwise
+    """
     try:
         float(string)
         return True
     except ValueError:
         return False
 
+
 def parse_coeffs(kpp_str):
+    """
+    Parse coefficients from a KPP string
+
+    Parameters
+        (str) kpp_str: f(x, y, z, ...)
+
+    Returns
+        (list of float): [float(x), float(y), float(z), ...]
+    """
 
     logging.debug(kpp_str)
 
@@ -30,7 +49,6 @@ def parse_coeffs(kpp_str):
             = coeff_str.replace(' ', '').replace('_dp', '').replace('D', 'E')
         if (is_float(coeff_str_reform)):
             coeffs.append(float(coeff_str_reform))
-        # coeffs.append(float(coeff_str_reform))
 
     logging.debug(coeffs)
 
