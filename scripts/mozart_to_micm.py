715bfe92302aaeb5aa5a2d3170a5da564a0a26df
davidfillmore <fillmore.david.winslow@gmail.com>
2024-11-04 08:22:28 -0700

    Fixed Arrhenius B coefficient sign convention.

--- scripts/mozart_to_micm.py
+++ scripts/mozart_to_micm.py
@@ -220,11 +220,11 @@ def parse_equation_set(label, lines):
         # typical 2 parameter coefficient values
         # 1e-12, 100 or -1000
         # MICM    A exp(C / T)
-        # Mozart  A exp(- B / T) ?
+        # Mozart  A exp(B / T)
         elif N_coeffs == 2:
             equation_dict['type'] = 'ARRHENIUS'
             equation_dict['A'] = float(coeffs_list[0])
-            equation_dict['C'] = - float(coeffs_list[1])
+            equation_dict['C'] = float(coeffs_list[1])
         # typical 5 parameter coefficient values
         # k0_A, ?, kinf_A, ?, Fc
         # 1e-33, 1, 1e-12, -1, 0.6
